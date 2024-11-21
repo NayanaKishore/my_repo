b1bc17df5df350a075287a1783eb90123f42a280
NayanaKishore <nayanadece@gmail.com>
2024-11-21 14:55:09 +0530

    this is a new commit

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class sample{
     public static void main(String [] args){
-        System.out.println("this is a sample java program");
+        int x=100;
+        System.out.println("This is a sample java program. The given value is "+ 100);
     }
 }
